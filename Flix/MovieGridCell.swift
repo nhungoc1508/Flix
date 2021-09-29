420ae517232b356978adc05321ade4d9b604d882
Ngoc N. Hoang <nnh245@nyu.edu>
2021-09-29 16:23:21 -0400

    Unit 2 basic requirements

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ngoc Hoang on 27/09/2021.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
